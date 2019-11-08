f1f965601e15a66314b9112b7498916dd88a31db
pastasauccen <jelau19@student.sdu.dk>
2019-11-08 10:03:27 +0100

    Anotoher test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey!ㅛㅐㅕ 솓ㄱㄷ!
+          Hey! You there!
         </p>
         <a
           className="App-link"
